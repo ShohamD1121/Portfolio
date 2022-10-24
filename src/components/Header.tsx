3cd90e55321577ff7bf2e6ef515a279f21d1b29a
shohamD1121 <shohamdouek@gmail.com>
2022-10-24 22:15:44 +0300

    small update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import myImg from "../images/my_pic.jpg"; 
+import myImg from "../images/my_pic.jpg";
 
 const Header: React.FC = () => {
   return (
@@ -26,7 +26,7 @@ const Header: React.FC = () => {
         </div>
         <motion.div
           whileInView={{ opacity: 0.5 }}
-          whileHover={{ scale: 1.05}}
+          whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.5, type: "tween" }}
           className="header_info_img"
         >
